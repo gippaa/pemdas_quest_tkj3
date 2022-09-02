d66821a6c81e12882df2e0eb32d2346d53f93cb5
gippaa <112367055+gippaa@users.noreply.github.com>
2022-09-02 10:21:57 +0700

    ghifana adzkia x tkj 3
    tugas python dasar

---
+++ python5-tkj3.py
@@ -0,0 +1,33 @@
+#Rumus Persegi sisi * sisi
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print ("luas persegi adalah", luas_p)
+
+#Rumus Segitiga A * T / 2
+alas_s = 20
+tinggi_s = 10
+luas_s = alas_s * tinggi_s / 2
+
+print ("luas persegi adalah", luas_s)
+
+#Rumus jajargenjang A * T
+alas_j = 10
+tinggi_j =15
+luas_j = alas_j * tinggi_j
+
+print ("luas jajar fgenjang adalah", luas_j)
+
+#Rumus lingkaran phi * r * r
+phi = 22/7
+r = 14
+luas_l = phi * r * r
+
+print ("luas lingkaran adalah", luas_l)
+
+print ("\n1.luas persegi")
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+
+print ("luas persegi adalah ", luas_p)
