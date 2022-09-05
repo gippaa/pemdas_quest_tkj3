e0da71aa23f15db6045540830ee7ec465744957b
gippaa <112367055+gippaa@users.noreply.github.com>
2022-09-05 12:11:47 +0700

    tugas pemdas

---
+++ python6-tkj3.py
@@ -0,0 +1,17 @@
+# Dolar ke rupiah
+dolar = int (input ("masukan nilai dolar: "))
+rupiah = dolar * 15000
+
+print ("dolar ke rupiah", rupiah)
+
+# Euro ke won
+euro = int(input("masukan nilai euro: "))
+won = euro * 1357
+
+print ("euro ke won", won)
+
+# Yen ke rupiah
+yen = int (input("masukan nilai yen"))
+rupiah = yen * 106
+
+print ("yen ke rupiah", rupiah)
