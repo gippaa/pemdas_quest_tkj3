7fc655fe52ce75ff2d7dc246355591524e1ecaab
gippaa <112367055+gippaa@users.noreply.github.com>
2022-09-09 07:24:40 +0700

    Add files via upload
    ghifana adzkia 10 tkj 3

---
+++ python8-tkj3.py
@@ -0,0 +1,10 @@
+List_kosong = []
+List_string = ["ghifana", "hastu", "zhafran","devan"]
+#                  0       1          2        3
+List_integer = [30, 24, 11, 5] 
+List_mix = ["ghifana", 30, True, 41.7]
+
+print("Nama berikut", List_string[0], "tanggal lahir",List_integer[0])
+print("Nama berikut", List_string[1], "tanggal lahir",List_integer[1])
+print("Nama berikut", List_string[2], "tanggal lahir",List_integer[2])
+print("Nama berikut", List_string[3], "tanggal lahir",List_integer[3])
